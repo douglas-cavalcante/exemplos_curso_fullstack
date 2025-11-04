d9bcb41e847459714433ac736d66c530badafb98
Henrique Douglas <henriquedouglas2803@gmail.com>
2025-11-03 22:06:30 -0300

    Adiciona lógica para cálculo de IMC e exibe resultado formatado

--- semana01/exercicio7.js
+++ semana01/exercicio7.js
@@ -1,5 +1,14 @@
+const readlineSync = require("readline-sync");
 
-
-function calcularIMC(){
-    
+function calcularIMC(peso, altura) {
+  var imc = peso / (altura * altura);
+  return imc
 }
+
+ var peso = readlineSync.questionFloat("Qual seu Peso?");
+ var altura = readlineSync.questionFloat("Qual sua altura?");
+
+// var resultado = calcularIMC(peso, altura)
+ //console.log(resultado.toFixed(2))
+
+ console.log("Seu imc é: " + calcularIMC(peso, altura).toFixed(2))
