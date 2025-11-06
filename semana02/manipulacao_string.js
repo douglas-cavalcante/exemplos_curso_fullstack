5c667223e7c1f20661a284707c3a680c2abb1f51
Henrique Douglas <henriquedouglas2803@gmail.com>
2025-11-06 19:03:10 -0300

    adiciona exemplos de manipulacao

---
+++ semana02/manipulacao_string.js
@@ -0,0 +1,54 @@
+
+/* Exemplo do metodo length
+
+var nome = "Henrique D";
+console.log("Valor da variavel", nome.length);
+if (nome.length == 10) {
+  console.log("Tamanho ok");
+}
+*/
+
+/* Exemplo do metodo toUpperCase e toLowerCase
+var nomePet = "totó"
+var nomePetMaiscula = nomePet.toUpperCase()
+
+console.log("Maiscula: " + nomePet.toUpperCase())
+console.log("Miniscula: " + nomePet.toLowerCase())
+
+console.log(nomePet)
+*/
+
+/*
+var usuario = "Henrique Douglas Cavalcante Costa"
+var usuarioTamanho = usuario.length 
+
+console.log("primeira letra " + usuario[0])
+console.log("ultima letra " + usuario[usuarioTamanho - 1])
+*/
+
+/*
+var minhaSenha = "Henrique douglas".trim()
+console.log(minhaSenha.trim())
+*/
+
+/*
+var nome = "Henrique"
+console.log(nome.indexOf("E"))
+*/
+
+/*
+var frase = "Eu testo javascript"
+
+if(frase.includes("amo")) {
+   console.log("Encontrou a palavra amo")
+} else {
+  console.log("Nao Encontrou a palavra amo")
+}
+*/
+
+var mensagem = "Hoje eu nao vou programar em javascript. por que javascript e ruim"
+
+console.log(mensagem.replaceAll("javascript", "CENSURADO"))
+
+
+
